aae69f1b3ebf91b579bd18035c18c6b39b3d85c6
Norbert Annus <annus.norbert@gmail.com>
2020-05-27 01:29:22 +0100

    Remove dependency on address id for Waitrose

--- src/api/waitrose.js
+++ src/api/waitrose.js
@@ -1,9 +1,10 @@
 import fetch from 'node-fetch';
 
-import { WAITROSE_USERNAME, WAITROSE_PASSWORD, WAITROSE_ADDRESS_ID } from '../config';
+import { WAITROSE_USERNAME, WAITROSE_PASSWORD } from '../config';
 import { WEEK_IN_MS, toDateString } from '../utils/date';
 
-const URL = 'https://www.waitrose.com/api/graphql-prod/graph/live';
+const GRAPHQL_URL = 'https://www.waitrose.com/api/graphql-prod/graph/live';
+const ADDRESSES_URL = 'https://www.waitrose.com/api/address-prod/v1/addresses';
 
 const getToken = async (username, password) => {
     const graphql = JSON.stringify({
@@ -11,6 +12,8 @@ const getToken = async (username, password) => {
             mutation($session: SessionInput) {
                 generateSession(session: $session) {
                     accessToken
+                    customerOrderId
+                    defaultBranchId
                 }
             }
         `,
@@ -23,21 +26,32 @@ const getToken = async (username, password) => {
             },
         },
     });
-    const result = await fetch(URL, {
+    const result = await fetch(GRAPHQL_URL, {
         body: graphql,
         headers: { 'Content-Type': 'application/json' },
         method: 'POST',
     });
     const {
-        data: {
-            generateSession: { accessToken },
-        },
+        data: { generateSession },
     } = await result.json();
 
-    return accessToken;
+    return generateSession;
 };
 
-const getSlots = async (token, fromDate) => {
+const getAddressId = async (token) => {
+    const result = await fetch(ADDRESSES_URL, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+        method: 'GET',
+    });
+    const addresses = await result.json();
+    const activeAddress = addresses.find(({ addressStatus }) => addressStatus === 'ACTIVE');
+
+    return activeAddress && activeAddress.id;
+};
+
+const getSlots = async ({ addressId, branchId, customerOrderId, token, fromDate }) => {
     const graphql = JSON.stringify({
         query: `
             query slotDays($slotDaysInput: SlotDaysInput) {
@@ -56,16 +70,16 @@ const getSlots = async (token, fromDate) => {
         `,
         variables: {
             slotDaysInput: {
-                branchId: '753',
+                branchId,
                 slotType: 'DELIVERY',
-                customerOrderId: '103580862',
-                addressId: WAITROSE_ADDRESS_ID,
+                customerOrderId,
+                addressId,
                 fromDate,
                 size: 7,
             },
         },
     });
-    const result = await fetch(URL, {
+    const result = await fetch(GRAPHQL_URL, {
         body: graphql,
         headers: {
             Authorization: `Bearer ${token}`,
@@ -85,12 +99,15 @@ const getSlots = async (token, fromDate) => {
 const filterFunction = ({ status }) => status !== 'FULLY_BOOKED' && status !== 'UNAVAILABLE';
 
 export const findAllSlots = async () => {
-    const token = await getToken(WAITROSE_USERNAME, WAITROSE_PASSWORD);
+    const { accessToken, customerOrderId, defaultBranchId } = await getToken(WAITROSE_USERNAME, WAITROSE_PASSWORD);
+    const addressId = await getAddressId(accessToken);
     const now = Date.now();
     const dates = Array.from({ length: 4 })
         .map((_, i) => new Date(now + i * WEEK_IN_MS))
         .map(toDateString);
-    const promises = dates.map((date) => getSlots(token, date));
+    const promises = dates.map((date) =>
+        getSlots({ addressId, branchId: defaultBranchId, customerOrderId, fromDate: date, token: accessToken })
+    );
     const results = await Promise.all(promises);
     const filteredResults = results
         .reduce((acc, curr) => {
